22782cd4ba865171d5c6da38698dcc22b420375c
Yovsky <mouloudguenane651@gmail.com>
2026-09-05 20:45:06 +0100

    feat: Added start and stop methods for WinDivert handle

--- src/network/windivertmanager.cpp
+++ src/network/windivertmanager.cpp
@@ -3,3 +3,49 @@
 WinDivertManager::WinDivertManager(QObject *parent)
     : QObject{parent}
 {}
+
+bool WinDivertManager::start()
+{
+    m_handle = WinDivertOpen(       // Populate the handle variable.
+        "tcp",
+        WINDIVERT_LAYER_NETWORK,
+        0,
+        0);
+
+    if (!m_handle)                  // Guard if the handle is unvalid.
+    {
+        qDebug()
+            << "Failed to open WinDivert handle: "
+            << GetLastError();
+        return false;
+    }
+
+    m_running = true;               // Mark WinDivert as running.
+    run();                          // Call the run function.
+
+    return true;
+}
+
+void WinDivertManager::stop()
+{
+    m_running = false;                  // Mark WinDivert as not running.
+
+    if (m_handle)                       // Check if the handle is valid.
+    {
+        if (WinDivertClose(m_handle))   // Try closing the handle.
+        {
+            m_handle = nullptr;         // If successful free the handle variable.
+        }
+        else
+        {
+            qDebug()                    // If failed print the error code.
+                << "Failed to close WinDivert handle: "
+                << GetLastError();
+        }
+    }
+}
+
+WinDivertManager::~WinDivertManager()   // Stop the handle when the destructor is called.
+{
+    stop();                             // Call the stop function.
+}
--- src/network/windivertmanager.h
+++ src/network/windivertmanager.h
@@ -1,15 +1,30 @@
 #ifndef WINDIVERTMANAGER_H
 #define WINDIVERTMANAGER_H
 
+#include "third-party/WinDivert/include/windivert.h"
+
 #include <QObject>
+#include <qDebug>
 
 class WinDivertManager : public QObject
 {
     Q_OBJECT
 public:
     explicit WinDivertManager(QObject *parent = nullptr);
+    ~WinDivertManager();
+
+    bool start();
+    void stop();
 
 signals:
+
+private:
+    // Functions
+    void run();
+
+    // Variables
+    void *m_handle = nullptr;
+    bool m_running = false;
 };
 
 #endif // WINDIVERTMANAGER_H
