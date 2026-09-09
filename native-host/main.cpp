484b9aa08c71ab257bccc8f315650f83f881508a
Yovsky <mouloudguenane651@gmail.com>
2026-09-09 10:47:01 +0100

    feat: Added windows binary mode handling for native messages

--- native-host/main.cpp
+++ native-host/main.cpp
@@ -1,3 +1,5 @@
+#include <io.h>
+#include <fcntl.h>
 #include <QCoreApplication>
 #include <QJsonObject>
 
@@ -7,7 +9,13 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    NativeMessaging *nMsg;                  // Create native messaging class.
+#ifdef _WIN32                               // Handle windows binary mode.
+    _setmode(_fileno(stdin), _O_BINARY);
+    _setmode(_fileno(stdout), _O_BINARY);
+#endif
+
+    NativeMessaging *nMsg                   // Create native messaging class pointer.
+        = new NativeMessaging();
 
     QJsonObject message;                    // Create the message json object.
     while (nMsg->readMessage(message))      // Loop to read messages.
